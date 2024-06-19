ee77f575ba3fdd382890b91598c61d670c81e887
Paul Donovan <paul.m.donovan@outlook.com>
2024-06-19 18:24:23 +1000

    commitschanges

---
+++ src/context/user.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+// create a provider component
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null)
+
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>{children}
+        </UserContext.Provider>
+    );
+
+}
+
+export { UserContext, UserProvider };
